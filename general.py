55cb4a2c386722801369ad510bdaad306925c4d6
Chino Franco <chino.franco@gmail.com>
2021-11-08 22:33:44 +0900

    Minor edits to functions

--- general.py
+++ general.py
@@ -1,7 +1,10 @@
 import time
+import math
 
-BOARD_WIDTH = 78
-BOARD_LENGTH = 117
+
+class Board(object):
+    WIDTH = 78
+    LENGTH = 117
 
 
 def timer(func):
@@ -15,17 +18,50 @@ def timer(func):
     return timer_wrapper
 
 
+def time_interval(start_period: float):
+    """
+    Calculates time interval, given a starting time.
+
+    :param start_period: Time of start
+    :return:
+    """
+    def convert_time_diff(duration):
+        duration = round(duration, 2)
+        d_hours = int(duration // 3600)
+        d_minutes = int((duration - (d_hours * 3600)) // 60)
+        d_seconds = int(round(duration - (d_hours * 3600) - (d_minutes * 60)))
+
+        return d_hours, d_minutes, d_seconds
+
+    try:
+        hours, minutes, seconds = convert_time_diff(time.time() - start_period)
+        time_elapsed = []
+
+        if hours:
+            time_elapsed.append(f'{hours}h')
+        if minutes:
+            time_elapsed.append(f'{minutes}m')
+        if seconds:
+            time_elapsed.append(f'{seconds}s')
+
+        return str(' '.join(time_elapsed))
+
+    except Exception as e:
+        print(f'Time interval computation error: {e}')
+        return "ERROR"
+
+
 def slope(pos1: tuple, pos2: tuple) -> float:
     num = pos2[1] - pos1[1]
     den = pos2[0] - pos1[0]
 
-    return round(num/den, 2)
+    return round(num / den, 2)
 
 
 def differentiate(data_set: list) -> list:
     diff_data = []
-    for i in range(1, len(data_set)+1):
-        v_r = slope(data_set[i], data_set[i-1])
+    for i in range(1, len(data_set) + 1):
+        v_r = slope(data_set[i], data_set[i - 1])
         diff_data.append(v_r)
 
     return diff_data
