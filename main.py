8bba73d724089dfbe8adc6c6d248a2d0d660b994
Chino Franco <chino.franco@gmail.com>
2021-11-10 00:07:30 +0900

    Description edits

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 DESIGN PRACTICE 2: Fall 2021 Semester
 Nagoya University, G30 Undergraduate Programs
 Automotive Engineering
-------------------------
+-----------------------
 Choi, Franco, Giovanni, Nguyen, Nazirjonov
 """
 from balltrack import BallTracker
-from testing import trial_scan
+from testfunctions import trial_scan
 from videoscan import scan_video
+
 
 if __name__ == "__main__":
     ballcam = BallTracker("ramp_trial.mp4")
