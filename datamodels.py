93897377fb815122997670da461c055308db6888
Chino Franco <chino.franco@gmail.com>
2021-12-06 00:22:27 +0900

    Created Region and Vector datatypes

--- datamodels.py
+++ datamodels.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 from pydantic import BaseModel, validator
 
 
@@ -6,23 +7,62 @@ class Board(object):
     """
     Store constants for project board.
     """
-    WIDTH = 78
-    LENGTH = 117
+    HEIGHT = 78
+    WIDTH = 117
     r_width = 0
     r_length = 0
+
+
+class Vector(BaseModel):
+    x: float
+    y: float
+
+    def __repr__(self):
+        return f'<Vector: x={self.x}, y={self.y}>'
+
+    def __str__(self):
+        return f'V({self.x:.2f}, {self.y:.2f})'
+
+    def __len__(self):
+        return round(self.resultant())
+
+    def resultant(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def angle(self):
+        return np.rad2deg(math.atan2(self.y, self.x))
+
+
+class Region(BaseModel):
+    """
+    Data class representing frame sizes.
+    """
+    width: tuple
+    length: tuple
+
+    def __repr__(self):
+        return f'<Region: width={self.width}, length={self.length}>'
+
+    def __len__(self):
+        r_x = self.width[1] - self.width[0]
+        r_y = self.length[1] - self.length[0]
+        return round(math.sqrt(r_x**2 + r_y**2))
 
 
 class Point(BaseModel):
     """
     Data class representing each recorded data point taken
-    from the video capture. Stores coordinates and timestamp.
+    from the video capture. Stores coordinate and timestamp.
     """
     x: int
     y: int
     time: float
 
     def __repr__(self):
-        return f'<Point position=({self.x},{self.y}) time={self.time:.3f}>'
+        return f'<Point: x={self.x}, y={self.y}, time={self.time:.3f}>'
+
+    def __str__(self):
+        return f'[P({self.x}, {self.y}) t={self.time:.3f}]'
 
     def __len__(self):
         return math.sqrt(self.x**2 + self.y**2)
